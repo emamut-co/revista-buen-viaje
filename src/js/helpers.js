e73fc9c5169a96db99432f5cbe4329c62cfd1efd
Faber Vergara <fabervergara@gmail.com>
2020-06-07 09:48:58 -0500

    add decrease logo width

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
-  $(window).load(function () {
-    $('.loader').fadeOut();
+  $(document).scroll(function () {
+    var transition = 'width 200ms ease-in-out, height 200ms ease-in-out'
+
+    $('#logo').css({ width: $(this).scrollTop() > 100 ? "50%" : "100%", transition: transition });
   });
 });
