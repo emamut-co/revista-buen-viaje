543c154c6c6e877eae15e5f8fe0e24dc441f296e
Faber Vergara <fabervergara@gmail.com>
2020-06-07 08:53:58 -0500

    get posts from articulo central category

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
-<?php get_header(); ?>
+<?php get_header();
+
+$args = array(
+  'category_id' => 30,
+  'posts_per_page' => 3,
+);
+
+$result = new WP_Query($args);
+$articuloCentralArray = $result->posts; ?>
 
 <section class="bg-light p-4">
   <div class="container">
     <div class="row">
       <div class="col-md-8">
-        <h2 class="title">Et occaecat exercitation consequat labore aute.</h2>
-        <img src='https://unsplash.it/950/650?random&blur&gravity=center' class="img-fluid mt-2" alt='' />
-        <p class="card-text mt-3"><small class="muted"><i class="far fa-clock"></i> Publicado: 15 de mayo de 2020</small><br>
-        Cillum qui magna ut eu id reprehenderit excepteur cillum duis anim nulla. Elit aliquip qui excepteur nisi do adipisicing ullamco nostrud nisi. Anim adipisicing nulla veniam magna velit laborum enim cillum esse sit esse elit voluptate Lorem.</small></p>
+        <h2 class="title"><?php echo $articuloCentralArray[0]->post_title ?></h2>
+        <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[0]->ID, 'full') ?>' class="img-fluid mt-2" alt='' />
+        <p class="card-text mt-3"><small class="muted"><i class="far fa-clock"></i> Publicado: <?php echo date_i18n('d \d\e F Y g:i', strtotime($articuloCentralArray[0]->post_date)) ?></small><br>
+        <?php echo !empty($articuloCentralArray[0]->excerpt) ? $articuloCentralArray[0]->excerpt : substr($articuloCentralArray[0]->post_content, 0, 250) . ' ...' ?></small></p>
         <p class="text-right text-muted"><a href="./single.php"><small>Leer más <i class="fa fa-angle-double-right"></i></small></a></p>
       </div>
       <div class="col-md-4">
         <input class="form-control mb-4" type="text" placeholder="Buscar...">
         <img src="<?php echo get_template_directory_uri() ?>/img/logo-articulo-central.png" alt="" class="img-fluid">
-        <a href="./single.php"><h5 class="title mt-4">Aliquip adipisicing minim officia occaecat dolore.</h5></a>
-        <img src='https://unsplash.it/800/420?random&blur&gravity=center' class="img-fluid mt-" alt='' />
+        <a href="./single.php"><h5 class="title mt-4"><?php echo $articuloCentralArray[1]->post_title ?></h5></a>
+        <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[1]->ID, 'full') ?>' class="img-fluid mt-" alt='' />
 
-        <a href="./single.php"><h5 class="title mt-5">Sit eiusmod sit nisi velit est tempor laboris.</h5></a>
-        <img src='https://unsplash.it/800/420?random&blur&gravity=center' class="img-fluid mt-" alt='' />
+        <a href="./single.php"><h5 class="title mt-5"><?php echo $articuloCentralArray[2]->post_title ?></h5></a>
+        <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[2]->ID, 'full') ?>' class="img-fluid mt-" alt='' />
       </div>
     </div>
   </div>
