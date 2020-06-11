7c8eab317597b5c547f30db30a48b79307a4b1bb
Faber Vergara <emamut@Fabers-MacBook-Pro.local>
2020-06-11 07:11:34 -0500

    fix home

--- index.php
+++ index.php
@@ -2,25 +2,41 @@
 
 $do_not_duplicate = array();
 $result = new WP_Query("category_name='articulo-central'&posts_per_page=3");
-$articuloCentralArray = $result->posts; ?>
+$articuloCentralArray = $result->posts;
+
+shuffle($articuloCentralArray) ?>
 
 <section class="container p-4 bg-light">
   <div class="row">
     <div class="col-md-8">
-      <a href="<?php echo get_permalink($articuloCentralArray[0]->ID) ?>"><h2 class="title"><?php echo $articuloCentralArray[0]->post_title; $do_not_duplicate[] = $articuloCentralArray[0]->ID ?></h2></a>
-      <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[0]->ID, 'full') ?>' class="img-fluid mt-2" alt='' />
-      <p class="card-text mt-3"><small class="muted"><i class="far fa-clock"></i> Publicado: <?php echo date_i18n('d \d\e F Y g:i', strtotime($articuloCentralArray[0]->post_date)) ?></small><br>
-      <?php echo get_the_excerpt($articuloCentralArray[0]->ID) ?></small></p>
-      <p class="text-right text-muted"><a href="<?php echo get_permalink($articuloCentralArray[0]->ID) ?>"><small>Leer más <i class="fa fa-angle-double-right"></i></small></a></p>
+      <a href="<?php echo get_permalink($articuloCentralArray[0]->ID) ?>">
+        <h2 class="title"><?php echo $articuloCentralArray[0]->post_title; $do_not_duplicate[] = $articuloCentralArray[0]->ID ?></h2>
+      </a>
+      <a href="<?php echo get_permalink($articuloCentralArray[0]->ID) ?>">
+        <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[0]->ID, 'full') ?>' class="img-fluid mt-2" alt='' />
+      </a>
+      <p class="card-text mt-3">
+        <small class="muted"><i class="far fa-clock"></i> Publicado: <?php echo date_i18n('d \d\e F Y g:i', strtotime($articuloCentralArray[0]->post_date)) ?></small><br>
+        <?php echo get_the_excerpt($articuloCentralArray[0]->ID) ?></small></p>
+      </p>
     </div>
     <div class="col-md-4">
-      <input class="form-control mb-4" type="text" placeholder="Buscar...">
+      <!-- <input class="form-control mb-4" type="text" placeholder="Buscar..."> -->
       <img src="<?php echo get_template_directory_uri() ?>/img/logo-articulo-central.png" alt="" class="img-fluid">
-      <a href="<?php echo get_permalink($articuloCentralArray[1]->ID) ?>"><h5 class="title mt-4"><?php echo $articuloCentralArray[1]->post_title; $do_not_duplicate[] = $articuloCentralArray[1]->ID ?></h5></a>
-      <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[1]->ID, 'full') ?>' class="img-fluid mt-" alt='' />
 
-      <a href="<?php echo get_permalink($articuloCentralArray[2]->ID) ?>"><h5 class="title mt-5"><?php echo $articuloCentralArray[2]->post_title; $do_not_duplicate[] = $articuloCentralArray[2]->ID ?></h5></a>
-      <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[2]->ID, 'full') ?>' class="img-fluid mt-" alt='' />
+      <a href="<?php echo get_permalink($articuloCentralArray[1]->ID) ?>">
+        <h5 class="title mt-4"><?php echo $articuloCentralArray[1]->post_title; $do_not_duplicate[] = $articuloCentralArray[1]->ID ?></h5>
+      </a>
+      <a href="<?php echo get_permalink($articuloCentralArray[1]->ID) ?>">
+        <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[1]->ID, 'full') ?>' class="img-fluid mt-" alt='' />
+      </a>
+
+      <a href="<?php echo get_permalink($articuloCentralArray[2]->ID) ?>">
+        <h5 class="title mt-5"><?php echo $articuloCentralArray[2]->post_title; $do_not_duplicate[] = $articuloCentralArray[2]->ID ?></h5>
+      </a>
+      <a href="<?php echo get_permalink($articuloCentralArray[2]->ID) ?>">
+        <img src='<?php echo get_the_post_thumbnail_url($articuloCentralArray[2]->ID, 'full') ?>' class="img-fluid mt-" alt='' />
+      </a>
     </div>
   </div>
 </section>
@@ -39,12 +55,11 @@ $articuloCentralArray = $result->posts; ?>
             while ( $noticiasNacionales->have_posts() ) : $noticiasNacionales->the_post(); $do_not_duplicate[] = get_the_ID(); ?>
             <div class="col mb-4">
               <div class="card border-0">
-                <?php the_post_thumbnail('medium', ['class' => 'card-img-top']) ?>
+                <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium', ['class' => 'card-img-top']) ?></a>
                 <div class="card-body px-0">
-                  <a class="card-title" href="<?php the_permalink() ?>"><strong><?php the_title() ?></strong></a>
-                  <p class="card-text"><small class="muted"><i class="far fa-clock"></i> Publicado: <?php the_time('d \d\e F Y g:i'); ?></small><br>
-                  <?php the_excerpt() ?></p>
-                  <p class="text-right text-muted"><a href="<?php the_permalink() ?>"><small>Leer más <i class="fa fa-angle-double-right"></i></small></a></p>
+                  <a href="<?php the_permalink() ?>">
+                    <h4 class="card-title"><?php the_title() ?></h4>
+                  </a>
                 </div>
               </div>
             </div>
@@ -62,12 +77,13 @@ $articuloCentralArray = $result->posts; ?>
             while ( $carroceros->have_posts() ) : $carroceros->the_post(); $do_not_duplicate[] = get_the_ID(); ?>
             <div class="col mb-4">
               <div class="card border-0">
-                <?php the_post_thumbnail('medium', ['class' => 'card-img-top']) ?>
-                <div class="card-body text-white px-0 bg-dark">
-                  <a class="card-title text-white" href="<?php the_permalink() ?>"><strong><?php the_title() ?></strong></a>
-                  <p class="card-text"><small class="muted"><i class="far fa-clock"></i> Publicado: <?php the_time('d \d\e F Y g:i'); ?></small><br>
-                    <?php the_excerpt() ?></p>
-                  <p class="text-right text-muted"><a href="<?php the_permalink() ?>"><small class="text-secondary">Leer más <i class="fa fa-angle-double-right"></i></small></a></p>
+                <a href="<?php the_permalink() ?>">
+                  <?php the_post_thumbnail('medium', ['class' => 'card-img-top']) ?>
+                </a>
+                <div class="card-body px-0 bg-dark">
+                  <a href="<?php the_permalink() ?>">
+                    <h4 class="card-title text-white"><?php the_title() ?></h4>
+                  </a>
                 </div>
               </div>
             </div>
@@ -85,12 +101,11 @@ $articuloCentralArray = $result->posts; ?>
             while ( $sobreLaMarcha->have_posts() ) : $sobreLaMarcha->the_post(); $do_not_duplicate[] = get_the_ID(); ?>
             <div class="col mb-4">
               <div class="card border-0">
-                <?php the_post_thumbnail('medium', ['class' => 'card-img-top']) ?>
+                <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium', ['class' => 'card-img-top']) ?></a>
                 <div class="card-body px-0">
-                  <a class="card-title" href="<?php the_permalink() ?>"><strong><?php the_title() ?></strong></a>
-                  <p class="card-text"><small class="muted"><i class="far fa-clock"></i> Publicado: <?php the_time('d \d\e F Y g:i'); ?></small><br>
-                  <?php the_excerpt() ?></p>
-                  <p class="text-right text-muted"><a href="<?php the_permalink() ?>"><small>Leer más <i class="fa fa-angle-double-right"></i></small></a></p>
+                  <a href="<?php the_permalink() ?>">
+                    <h4 class="card-title"><?php the_title() ?></h4>
+                  </a>
                 </div>
               </div>
             </div>
