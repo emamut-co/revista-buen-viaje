b659fd2639ac4683489ed5e1d30a1ff1d8596ca2
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-06-22 06:53:44 -0500

    starts fix layout

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
-      <footer class="bg-dark py-5">
-        <div class="row">
+      <footer>
+        <div class="row bg-secondary py-5">
+          <div class="col text-center">
+            <h3>Descarga nuestro PDF</h3>
+          </div>
+        </div>
+        <div class="row bg-dark py-5">
           <div class="col text-center">
             <h3 class="text-white"><strong>REVISTA BUEN VIAJE</strong></h3>
             <h6 class="text-white font-roboto"><i class="fas fa-map-marker-alt text-red"></i> Gonzalo Pizarro s/n y Pasaje Las Vegas Churoloma - Tumbaco - Quito</h6>
