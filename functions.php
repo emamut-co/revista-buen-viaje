200c43955a91d9e72f91dd7d8892c2a90f15cd37
Faber Vergara <fabervergara@gmail.com>
2020-08-19 18:22:18 -0500

    add responsive class to images on content

--- functions.php
+++ functions.php
@@ -136,3 +136,19 @@ function theme_get_custom_logo() {
   else
     echo '<h1>' . get_bloginfo( 'name' ) . '</h1>';
 }
+
+function add_responsive_class($content){
+  $content = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
+  $document = new DOMDocument();
+  libxml_use_internal_errors(true);
+  $document->loadHTML(utf8_decode($content));
+
+  $imgs = $document->getElementsByTagName('img');
+  foreach ($imgs as $img) {
+      $img->setAttribute('class','img-fluid');
+  }
+
+  $html = $document->saveHTML();
+  return $html;
+}
+add_filter ('the_content', 'add_responsive_class');
